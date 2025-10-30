2b6ec60573c9e2094588c64714e5b734ac0f2760
LoganRS230 <logarisal2.1@gmail.com>
2025-10-30 17:41:13 -0500

    un guardado de lo que quedo hecho antes de viajar 12 horas, hoy solo me dedicaré a solucionar posibles errores y a probar cada endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 const express = require('express');
-const router = express.Router();
 const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
-const resenasRoutes = require('./routes/resenasRoutes');
 
 dotenv.config();
 connectDB();
